2810850de8fbea018fc509c4a660d0867cf01756
yuyunaginagi <centrepg8@gmail.com>
2024-06-07 22:03:47 +0900

    CSSによる装飾

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,7 +3,13 @@ import React from "react";
 export default class Index extends React.Component {
   render() {
     return (
-      <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
         <h1>ジャンケンに挑戦しよう！</h1>
       </div>
     );
