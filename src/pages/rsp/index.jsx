18cb22bc7989e4543cf87bff18ada52132b74ca8
yuyunaginagi <centrepg8@gmail.com>
2024-06-10 15:28:09 +0900

    ビュー編集

--- src/pages/rsp/index.jsx
+++ src/pages/rsp/index.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 export default class Index extends React.Component {
   render() {
+    const imgSize = 100;
     return (
       <div
         style={{
@@ -12,6 +13,38 @@ export default class Index extends React.Component {
         }}
       >
         <h1>じゃんけんページ</h1>
+        <div style={{ display: "flex" }}>
+          <button>
+            <img
+              src={`images/rock.jpeg`}
+              alt="rock"
+              style={{
+                height: imgSize,
+                width: imgSize,
+              }}
+            />
+          </button>
+          <button>
+            <img
+              src={`images/paper.jpeg`}
+              alt="paper"
+              style={{
+                height: imgSize,
+                width: imgSize,
+              }}
+            />
+          </button>
+          <button>
+            <img
+              src={`images/scissors.jpeg`}
+              alt="scissors"
+              style={{
+                height: imgSize,
+                width: imgSize,
+              }}
+            />
+          </button>
+        </div>
         <Link to="/">
           <h1>じゃんけんを終了する</h1>
         </Link>
