d409ce31d178c3071ecc90b57107956419f79b98
yuyunaginagi <centrepg8@gmail.com>
2024-06-15 14:01:45 +0900

    関数コンポーネントへの書き換え

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default class Index extends React.Component {
-  render() {
-    return (
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <h1>ジャンケンに挑戦しよう！</h1>
-        <Link to ="/rsp">
-          <h1>ジャンケンページへ</h1>
-        </Link>
-      </div>
-    );
-  }
+export default function Index() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h1>ジャンケンに挑戦しよう！</h1>
+      <Link to ="/rsp">
+        <h1>ジャンケンページへ</h1>
+      </Link>
+    </div>
+  );
 }
